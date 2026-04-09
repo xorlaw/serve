4e43384edf4789051d6b62d5a7559ed6484bd80f
xor <xor@morke.org>
2026-04-09 16:09:35 +0100

    zero-init sockaddr_in, bump backlog to 10 in server.h

--- src/server.h
+++ src/server.h
@@ -9,36 +9,40 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
-#define BACKLOG 5
+#define PORT    8080
+#define BACKLOG 10
 #define BUFFER_SIZE 4096
 
 int create_server_socket(void) {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket failed");
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
         exit(EXIT_FAILURE);
     }
 
     int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt failed");
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK); 
-    address.sin_port = htons(PORT);
+    struct sockaddr_in addr = {0};
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_port        = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(fd, BACKLOG) < 0) {
+        perror("listen");
         exit(EXIT_FAILURE);
     }
 
     printf("server listening on http://localhost:%d\n", PORT);
-    return server_fd;
+    return fd;
 }
 
 #endif
-
